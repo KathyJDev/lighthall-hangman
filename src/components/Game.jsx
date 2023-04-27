d42f23198edc226a1cacd9c04ee4eab23d95470b
Kathryn Jerez <kathrynjerez@gmail.com>
2023-04-27 18:35:20 -0400

    add hint button

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -13,29 +13,39 @@ import { doc, getDoc } from "firebase/firestore";
 import Modal from "./Modal";
 import { toast } from "react-toastify";
 
-const words = [
-  "monitor",
-  "program",
-  "application",
-  "keyboard",
-  "javascript",
-  "gaming",
-  "network",
-  "application",
-  "programming",
-  "interface",
-  "wizard",
-];
+const words = ["monitor", "program", "application", "keyboard", "javascript", "gaming", "network", "interface", "wizard", "server", "database", "algorithm", "debug", "cache", "firewall", "router", "cyberspace", "bandwidth", "code", "encryption", "hacker", "malware", "metadata", "protocol", "serverless", "spam", "telemetry", "upload", "virtualization", "wiki"]
 
 const hints = {
-  monitor: "A device used to display images or video",
-  program: "A set of instructions that a computer follows",
-  application: "A software designed to perform a specific function",
-  keyboard: "An input device used for typing characters",
-  javascript: "A programming language used to create interactive web pages",
-  gaming: "The activity of playing video games",
-  network: "A group of interconnected devices that communicate with each other",
-  interface: "The means by which a user interacts with a computer",
+  monitor: "a device for displaying visual output from a computer",
+  program: "a set of instructions that a computer follows to perform a task",
+  application: "a computer program designed for a specific purpose",
+  keyboard: "an input device used to enter text and commands into a computer",
+  javascript: "a programming language used to create interactive effects on websites",
+  gaming: "the activity of playing video games",
+  network: "a group of interconnected computers or devices",
+  interface: "a point of interaction between a computer system and a user",
+  wizard: "a software tool that guides users through a complex process",
+  server: "a computer or system that provides resources or services to other computers or devices",
+  database: "a collection of data that is organized for easy access, retrieval, and management",
+  algorithm: "a set of steps or rules used to solve a problem or perform a task",
+  debug: "the process of finding and fixing errors or problems in computer code",
+  cache: "a component that stores data so that future requests for that data can be served faster",
+  firewall: "a security system that monitors and controls incoming and outgoing network traffic",
+  router: "a networking device that forwards data packets between computer networks",
+  cyberspace: "the virtual environment created by computer systems and networks",
+  bandwidth: "the maximum amount of data that can be transmitted over a network or internet connection",
+  code: "a set of instructions or statements in a computer program",
+  encryption: "the process of converting information or data into a code to prevent unauthorized access",
+  hacker: "a person who uses computer systems to gain unauthorized access to data",
+  malware: "software that is designed to harm computer systems or data",
+  metadata: "information about other data, such as the format, author, or creation date",
+  protocol: "a set of rules for communicating data between computer systems",
+  serverless: "a model of computing where a third-party provider manages server infrastructure",
+  spam: "unwanted or unsolicited email messages",
+  telemetry: "the process of collecting and transmitting data from remote devices",
+  upload: "the process of transferring data from a local computer to a remote computer or server",
+  virtualization: "the creation of a virtual version of something, such as a computer system or network",
+  wiki: "a collaborative website that allows users to contribute and edit content"
 };
 //let selectedWord = words[Math.floor(Math.random() * words.length)];
 
