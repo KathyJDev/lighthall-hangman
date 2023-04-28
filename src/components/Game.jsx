385b7ec20787b4d1e667ea25b600cc0fadc1c65c
Salami <salamite37@gmail.com>
2023-04-28 18:29:38 +0100

    challenge friend bug fix

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -96,32 +96,32 @@ const Game = () => {
   };
 
   // for keyboard press
-  useEffect(() => {
-    const handleKeydown = (event) => {
-      const { key, keyCode } = event;
-      if (playable && keyCode >= 65 && keyCode <= 90) {
-        const letter = key.toLowerCase();
-        if (selectedWord.includes(letter)) {
-          if (!correctLetters.includes(letter)) {
-            setCorrectLetters((currentLetters) => [...currentLetters, letter]);
-          } else {
-            show(setShowNotification);
-          }
-        } else {
-          if (!wrongLetters.includes(letter)) {
-            setWrongLetters((currentLetters) => [...currentLetters, letter]);
-          } else {
-            show(setShowNotification);
-          }
-        }
-      }
-    };
-    if (isOpened === false) {
-      window.addEventListener("keydown", handleKeydown);
-    }
+  // useEffect(() => {
+  //   const handleKeydown = (event) => {
+  //     const { key, keyCode } = event;
+  //     if (playable && keyCode >= 65 && keyCode <= 90) {
+  //       const letter = key.toLowerCase();
+  //       if (selectedWord.includes(letter)) {
+  //         if (!correctLetters.includes(letter)) {
+  //           setCorrectLetters((currentLetters) => [...currentLetters, letter]);
+  //         } else {
+  //           show(setShowNotification);
+  //         }
+  //       } else {
+  //         if (!wrongLetters.includes(letter)) {
+  //           setWrongLetters((currentLetters) => [...currentLetters, letter]);
+  //         } else {
+  //           show(setShowNotification);
+  //         }
+  //       }
+  //     }
+  //   };
+  //   if (isOpened === false) {
+  //     window.addEventListener("keydown", handleKeydown);
+  //   }
 
-    return () => window.removeEventListener("keydown", handleKeydown);
-  }, [correctLetters, wrongLetters, playable]);
+  //   return () => window.removeEventListener("keydown", handleKeydown);
+  // }, [correctLetters, wrongLetters, playable]);
 
   function playAgain() {
     setPlayable(true);
